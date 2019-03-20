73d939d38fc772aae48e219766309cb418b6a68c
B3v3 <boksa.dawid@interia.pl>
2019-03-20 06:19:47 +0100

    fix errors request test

--- spec/request/errors_spec.rb
+++ spec/request/errors_spec.rb
@@ -3,15 +3,8 @@ require 'rails_helper'
 RSpec.describe 'errors', :type => :request do
   describe "not_found" do
     it "returns http success" do
-      get :not_found
-      expect(response).to have_http_status(404)
-    end
-  end
-
-  describe "internal_server_error" do
-    it "returns http success" do
-      get :internal_server_error
-      expect(response).to have_http_status(500)
+      get '/404'
+      expect(response.status).to eq(404)
     end
   end
 end
